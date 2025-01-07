be6f6a0e45df8fed63d79bc57804508403541a32
Temur Coder <temurnarzullayev810@gmail.com>
2025-01-07 18:12:30 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/ @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public//*.{html,js}", 'index.html'],
+  theme: {
+    extend: {
+      backgroundColor: {
+        bodyColor: '#FEF4F4',
+        mavbarColor: '#FEF4F4',
+        buttonColor: '#FF1616',
+        redColor: '#FF1616',
+        buttonColorHover: '#CF0F0F',
+        buttonColorHoverRed: '#FF1616',
+        whiteColor: '#fff',
+        sectionColorBlue: '#252E56',
+        colorMenu: '#4C4C4C',
+      },
+      borderColor: {
+        inpBorder: '#DEE0E6',
+        cardBorder: '#EEE1E0',
+      },
+      textColor: {
+        colorBlack: '#000',
+        colorWhite: '#fff',
+        colorGrey: '#949BAA',
+        colorGrey2: '#9097A7',
+        colorRed: '#FF1616',
+        colorBlack: '#232F55',
+      },
+    },
+  },
+  plugins: [],
+}
