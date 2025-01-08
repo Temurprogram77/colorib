d13000cc2e81d72a92f87367f30013e295eb23b4
Temur Coder <temurnarzullayev810@gmail.com>
2025-01-08 17:15:35 +0300

    slider js and css completed

--- js/slider.js
+++ js/slider.js
@@ -1,2 +1,8 @@
-var splide = new Splide( '.splide' );
+var splide = new Splide( '.splide', {
+    type: 'loop',
+    snap: true,
+    arrows: false,
+    autoplay: true,
+    perpage: 1,
+} );
 splide.mount();
