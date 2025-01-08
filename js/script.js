93a6f6f88dd058f31000b6889526da711bc8228b
Temur Coder <temurnarzullayev810@gmail.com>
2025-01-08 16:32:15 +0300

    navbar scroll >100

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const scrollHeader = () =>{
     const header = document.getElementById('navbar')
     // Add a class if the bottom offset is greater than 50 of the viewport
-    this.scrollY >= 50 ? header.classList.add('bg-header') 
+    this.scrollY >= 100 ? header.classList.add('bg-header') 
                        : header.classList.remove('bg-header')
 }
 window.addEventListener('scroll', scrollHeader)
