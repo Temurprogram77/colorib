c403507a956b78830195ec9059840c25d355f61f
Temur Coder <temurnarzullayev810@gmail.com>
2025-01-09 16:56:50 +0300

    add splide cards

--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,19 @@
-var splide = new Splide( '.splide', {
-    type: 'loop',
+document.addEventListener("DOMContentLoaded", function () {
+  var splide1 = new Splide(".splide", {
+    type: "loop",
     snap: true,
     arrows: false,
     autoplay: true,
     perpage: 1,
-} );
-splide.mount();
+  });
+  splide1.mount();
+
+  var splide2 = new Splide("#splide", {
+    perPage: 5,
+    autoplay: true,
+    snap: true,
+    type: "loop",
+    focus: "center",
+  });
+  splide2.mount();
+});
