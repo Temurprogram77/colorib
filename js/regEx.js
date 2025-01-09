06795d93f573bf75d4447eb41bae87e8585cef4e
Temur Coder <temurnarzullayev810@gmail.com>
2025-01-09 15:35:57 +0300

    add

---
+++ js/regEx.js
@@ -0,0 +1,16 @@
+let email = document.querySelector("#email");
+let password = document.querySelector("#password");
+let btn = document.querySelector("#btn");
+let wrong = document.querySelector(".wrong");
+
+btn.addEventListener("click", () => {
+  e.preventDefault();
+  const emailRegEx = /[a-z0-9]{6,12}@gmail/;
+  const passwordRegEx = /^[0-9]{8,12}$/;
+  const nameRegEx = /[a-zA-Z]/;
+  let emailValue = email.value;
+  let passwordValue = password.value;
+  if () {
+    
+  }
+});
