5de95958f131ec80700b1f9eb9f9c0c09d2b6a70
Temur Coder <temurnarzullayev810@gmail.com>
2025-01-09 16:26:33 +0300

    add regEx

--- js/regEx.js
+++ js/regEx.js
@@ -8,13 +8,14 @@ btn.addEventListener("click", () => {
 //   e.preventDefault();
   const emailRegEx = /[a-z0-9]{6,12}@gmail/;
   const passwordRegEx = /^[0-9]{8,12}$/;
-//   const nameRegEx = /[a-zA-Z]{8,12}/;
   let emailValue = email.value;
   let passwordValue = password.value;
   if (emailRegEx.test(emailValue) && passwordRegEx.test(passwordValue)) {
     window.location.href = '/index.html';
   } else {
-    wrong.textContent = "Please enter your email address"
-    wrong2.textContent = "Please enter your password"
+    email.style.border = "2px solid red" 
+    password.style.border = "2px solid red" 
+    wrong.textContent = "Please enter your email [a-z] [6-12] @gmail.com";
+    wrong2.textContent = "Please enter your password (only numbers [8-12])"
   }
 });
