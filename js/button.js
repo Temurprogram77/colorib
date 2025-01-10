eeeee19a87b867fdde092125f467fbb67064e313
Temur Coder <temurnarzullayev810@gmail.com>
2025-01-11 00:20:41 +0300

    button js completed

--- js/button.js
+++ js/button.js
@@ -1,14 +1,15 @@
 const email = localStorage.getItem("email");
-const loginBtn = document.getElementById("loginBtn");
-const welcomeMsg = document.getElementById("welcomeMsg");
+const sign = document.querySelector(".sign");
+const welcomeMsg = document.querySelector(".welcomeMsg");
 
 if (email) {
   // Agar foydalanuvchi login qilgan bo'lsa
   welcomeMsg.classList.remove("hidden"); // "Salom" yozuvini ko'rsatadi
+  sign.classList.add("hidden"); // "Salom" yozuvini ko'rsatadi
 } else {
   // Agar foydalanuvchi login qilmagan bo'lsa
-  loginBtn.classList.remove("hidden"); // "Login" tugmasini ko'rsatadi
-  loginBtn.addEventListener("click", function () {
-    window.location.href = "login.html"; // Login sahifasiga yo'naltirish
+  sign.classList.remove("hidden"); // "Login" tugmasini ko'rsatadi
+  sign.addEventListener("click", function () {
+    window.location.href = "/public/sign-in.html"; // Login sahifasiga yo'naltirish
   });
 }
