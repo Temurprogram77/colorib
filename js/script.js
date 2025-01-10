7376a5f004fa1fed0cbdc05cf6d384fbffa155c1
Temur Coder <temurnarzullayev810@gmail.com>
2025-01-10 16:38:34 +0300

    start inp modal

--- js/script.js
+++ js/script.js
@@ -5,3 +5,21 @@ const scrollHeader = () =>{
                        : header.classList.remove('bg-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+let inp = document.querySelector('#search-inp')
+let search_modal = document.querySelector('.search-modal') 
+let close_btn = document.querySelector('.close_btn') 
+
+inp.addEventListener('click', ()=>{
+    search_modal.classList.remove('hidden')
+    setTimeout(()=>{
+        search_modal.classList.add('translate-y-4')
+    },50)
+})
+
+close_btn.addEventListener('click', ()=>{
+    search_modal.classList.remove('translate-y-4')
+    setTimeout(()=>{
+        search_modal.classList.add('hidden')
+    },100)
+})
